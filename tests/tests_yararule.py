f8582d4164e434d86c16692fc7921c31410e4128
BitsOfBinary <48534004+BitsOfBinary@users.noreply.github.com>
2020-06-07 21:10:11 +0100

    Separated YaraRule from YaraBuilder

--- tests/tests_yararule.py
+++ tests/tests_yararule.py
@@ -1,17 +1,7 @@
 import unittest
-import collections
-import unittest.mock
 
-from yarabuilder import (
-    YaraRule,
-    YaraCondition,
-    YaraTags,
-    YaraImports,
-    YaraString,
-    YaraStrings,
-    YaraMeta,
-    YaraMetaEntry, YaraBuilder,
-)
+from yarabuilder.yararule import YaraRule, YaraCondition, YaraTags, YaraImports, YaraString, YaraStrings, \
+    YaraMeta, YaraMetaEntry
 
 
 class TestYaraRule(unittest.TestCase):
@@ -250,36 +240,3 @@ class TestYaraMetaEntry(unittest.TestCase):
         yara_meta_entry = YaraMetaEntry("test_value", 0)
         self.assertEqual(yara_meta_entry.value, "test_value")
         self.assertEqual(yara_meta_entry.position, 0)
-
-
-class TestYaraBuilder(unittest.TestCase):
-    @unittest.mock.patch('yarabuilder.YaraRule')
-    def setUp(self, mocked_yara_rule):
-        self.yara_builder = YaraBuilder()
-        self.yara_builder.yara_rules["test_rule"] = mocked_yara_rule
-
-    def test_no_rule_name_exception_handler(self):
-        self.assertRaises(KeyError, self.yara_builder.no_rule_name_exception_handler, "nonexistant_rule")
-
-    def test_create_rule(self):
-        self.yara_builder.create_rule("another_rule")
-        self.assertIsInstance(self.yara_builder.yara_rules["another_rule"], YaraRule)
-
-    def test_create_two_rules_with_same_name(self):
-        self.assertRaises(KeyError, self.yara_builder.create_rule, "test_rule")
-
-    def test_add_tag(self):
-        self.yara_builder.add_tag("test_rule", "test_tag")
-        self.yara_builder.yara_rules["test_rule"].tags.add_tag.assert_called_once_with("test_tag")
-
-    def test_add_import(self):
-        self.yara_builder.add_import("test_rule", "test_import")
-        self.yara_builder.yara_rules["test_rule"].imports.add_import.assert_called_once_with("test_import")
-
-    def test_add_meta_custom_type(self):
-        self.yara_builder.add_meta("test_rule", "test_meta_name", "test_meta_text", meta_type="custom")
-        self.yara_builder.yara_rules["test_rule"].meta.add_meta.assert_called_once_with("test_meta_name", "test_meta_text", meta_type="custom")
-
-    def test_add_meta_text(self):
-        self.yara_builder.add_meta("test_rule", "test_meta_name", "test_meta_text")
-        self.yara_builder.yara_rules["test_rule"].meta.add_meta.assert_called_once_with("test_meta_name", "test_meta_text", meta_type="text")
--- yarabuilder/yararule.py
+++ yarabuilder/yararule.py
@@ -1,5 +1,5 @@
-import logging
 import collections
+import logging
 
 
 class YaraMetaEntry:
@@ -225,51 +225,6 @@ class YaraRule:
             self.raw_rule = self.build_rule_condition_section(self.raw_rule)
 
             return self.raw_rule
-
-
-class YaraBuilder:
-    def __init__(self, ws="    "):
-        self.ws = ws
-        self.yara_rules = collections.OrderedDict()
-
-    def no_rule_name_exception_handler(self, rule_name):
-        if rule_name not in self.yara_rules:
-            raise KeyError("Rule  \"%s\" doesn't exist", rule_name)
-
-    def create_rule(self, rule_name):
-        if rule_name in self.yara_rules:
-            raise KeyError("Rule with name \"%s\" already exists", rule_name)
-
-        self.yara_rules[rule_name] = YaraRule(rule_name)
-
-    def add_tag(self, rule_name, tag):
-        self.no_rule_name_exception_handler(rule_name)
-        self.yara_rules[rule_name].tags.add_tag(tag)
-
-    def add_import(self, rule_name, import_str):
-        self.no_rule_name_exception_handler(rule_name)
-        self.yara_rules[rule_name].imports.add_import(import_str)
-
-    def add_meta(self, rule_name, name, value, meta_type=None):
-        self.no_rule_name_exception_handler(rule_name)
-
-        if meta_type:
-            self.yara_rules[rule_name].meta.add_meta(name, value, meta_type=meta_type)
-
-        elif isinstance(value, str):
-            self.yara_rules[rule_name].meta.add_meta(name, value, meta_type="text")
-
-    def add_string(self, rule_name, string):
-        pass
-
-    def add_condition(self, rule_name, condition):
-        pass
-
-    def build_rule(self, rule_name):
-        pass
-
-    def build_rules(self):
-        pass
 
 
 def main():  # pragma: no cover
