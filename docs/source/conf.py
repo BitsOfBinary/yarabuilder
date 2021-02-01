1bdbdafb0613af1ee00bd1cae40d6eb8e279b4d1
BitsOfBinary <48534004+BitsOfBinary@users.noreply.github.com>
2021-02-01 18:29:29 +0000

    Bump to v0.0.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, BitsOfBinary'
 author = 'BitsOfBinary'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '0.0.4'
 
 
 # -- General configuration ---------------------------------------------------
